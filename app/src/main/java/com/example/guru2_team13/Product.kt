0b6e42f7f85308e1c1ee8ccd8c55201257bd37e0
leastzero <csong7177@naver.com>
2023-07-22 01:08:31 +0900

    Product 화면 구성

---
+++ app/src/main/java/com/example/guru2_team13/Product.kt
@@ -0,0 +1,11 @@
+package com.example.guru2_team13
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Product : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product)
+    }
+}
