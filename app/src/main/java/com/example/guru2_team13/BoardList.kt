8365b644602bd78c47b2eaf12c4fc675ce997107
dktjstn <tomatochomp@gmail.com>
2023-07-28 09:51:17 +0900

    Board 레이아웃 완성

--- app/src/main/java/com/example/guru2_team13/BoardList.kt
+++ app/src/main/java/com/example/guru2_team13/BoardList.kt
@@ -4,18 +4,31 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class BoardList : AppCompatActivity() {
+    lateinit var fab_write: FloatingActionButton
     lateinit var recipeButton1: ImageButton
+    lateinit var recipeButton3: ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_board_list)
 
+        fab_write = findViewById(R.id.fab_write)
         recipeButton1 = findViewById(R.id.recipeButton1)
+        recipeButton3 = findViewById(R.id.recipeButton3)
 
+        fab_write.setOnClickListener {
+            var intent = Intent(this, BoardWrite::class.java)
+            startActivity(intent)
+        }
         recipeButton1.setOnClickListener {
             var intent = Intent(this, BoardDetail::class.java)
+            startActivity(intent)
+        }
+        recipeButton3.setOnClickListener {
+            var intent = Intent(this, BoardDetail2::class.java)
             startActivity(intent)
         }
     }
--- app/src/main/java/com/example/guru2_team13/BoardWrite.kt
+++ app/src/main/java/com/example/guru2_team13/BoardWrite.kt
@@ -1,11 +1,21 @@
 package com.example.guru2_team13
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class BoardWrite : AppCompatActivity() {
+    lateinit var fab_menu: FloatingActionButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_board_write)
+
+        fab_menu = findViewById(R.id.fab_menu)
+
+        fab_menu.setOnClickListener {
+            var intent = Intent(this, BoardList::class.java)
+            startActivity(intent)
+        }
     }
 }
